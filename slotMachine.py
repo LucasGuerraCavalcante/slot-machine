20254be8d94109f08ef208da7efe05a7f50dd2cc
LucasGuerraCavalcante <lucasguerratee@gmail.com>
2019-07-30 16:45:39 -0300

    Improving the game

--- slotMachine.py
+++ slotMachine.py
@@ -2,7 +2,7 @@ import random
 
 initialCoins = 50
 
-def slotMachine(coins = initialCoins):
+def playSlotMachine(coins):
 
     reel1 = ["7", "BAR", "BAR", "BAR", "WM", "WM", "BELL", "GP", "GP", "GP", "GP", "GP", "GP", "GP", "OR", "OR", "OR",
             "OR", "OR", "CH", "CH"]
@@ -20,14 +20,21 @@ def slotMachine(coins = initialCoins):
     if coins == 0:
         print("Out of coins! You can't play")
     else:
-        print("How many coins do you want to bet on the line?")
-        bet = int(input("BET: "))
-        coins = coins - bet
-        if bet <= 0:
-            print("Invalid value, try again")
-        else:
-            game = [reel1[random.randint(0,20)],reel2[random.randint(0,23)],reel3[random.randint(0,22)]]
-            return print(game), print(coins)
+        coinsInserted  = False
+        while coinsInserted  is False:
+            print("\nHow many coins do you want to bet on the line?")
+            bet = int(input("BET: "))
+            if bet <= 0:
+                print("Invalid value, try again")
+                coinsInserted  = False
+            elif bet <= coins:
+                coinsInserted  = True
+                coins = coins - bet
+                game = [reel1[random.randint(0,20)],reel2[random.randint(0,23)],reel3[random.randint(0,22)]]
+                return print(game), print(coins)
+            else:
+                print("You don't have enough coins")
+                coinsInserted  = False
 
 
-slotMachine()
+playSlotMachine(coins = initialCoins)
