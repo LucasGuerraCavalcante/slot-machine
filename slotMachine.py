85da7361da335d740a6d728c532cd57ee6e5adfe
LucasGuerraCavalcante <lucasguerratee@gmail.com>
2019-07-31 17:24:00 -0300

    Its not a function anymore

--- slotMachine.py
+++ slotMachine.py
@@ -2,85 +2,88 @@
 import os, sys
 import random as rd
 
-initialCoins = 50
+coins = 50
 
-def playSlotMachine(coins):
-
-    reel1 = ["🐍", # Python Snake (1)
-            "🤖", "🤖", "🤖", # Robot (3)
+reel1 = ["🐍", # Python Snake (1)
+            "💻", "💻", "💻", # Computer (3)
             "🍉", "🍉", # Watermelon (2)
             "🔔", # Bell (1)
             "🍇", "🍇", "🍇", "🍇", "🍇", "🍇", "🍇", # Grape (7)
             "🍊", "🍊", "🍊","🍊", "🍊", # Orange (5)
             "🍒", "🍒"] # Cherrie (2)
 
-    reel2 = ["🐍", # Python Snake (1)
-            "🤖", "🤖", # Robot (2)
+reel2 = ["🐍", # Python Snake (1)
+            "💻", "💻", # Computer (2)
             "🍉", "🍉", # Watermelon (2)
             "🔔", "🔔", "🔔", "🔔", "🔔", # Bell (5)
             "🍇", "🍇", "🍇", # Grape (3)
             "🍊", "🍊", "🍊","🍊", "🍊", # Orange (5)
             "🍒", "🍒","🍒", "🍒", "🍒", "🍒"] # Cherrie (6)
 
-    reel3 = ["🐍", # Python Snake (1)
-            "🤖", # Robot (1)
+reel3 = ["🐍", # Python Snake (1)
+            "💻", # Computer (1)
             "🍉", "🍉", # Watermelon (2)
             "🔔", "🔔", "🔔", "🔔", "🔔", "🔔", "🔔", "🔔", # Bell (8)
             "🍇", "🍇", "🍇", # Grape (3)
             "🍊", "🍊", "🍊", "🍊", # Orange (4)
             "👾", "👾", "👾", "👾"] # Evil Alien (4)
 
-    game = []
+game = []
 
-    if coins == 0:
-        print("Out of coins! You can't play")
-    else:
-        coinsInserted  = False
-        while coinsInserted  is False:
-            print("\nHow many coins do you want to bet on the line?")
-            bet = int(input("BET: "))
-            if bet <= 0:
-                print("\nInvalid value, try again")
-                coinsInserted  = False
-            elif bet <= coins:
-                coinsInserted  = True
-                coins = coins - bet
-                game = [reel1[rd.randint(0,20)],reel2[rd.randint(0,23)],reel3[rd.randint(0,22)]]
-                print("              .-------.")
-                print("              |       |")
-                print("  ____________|_______|____________")
-                print(" |  __    __    ___  _____   __    |")  
-                print(" | / _\  / /   /___\/__   \ / _\   |") 
-                print(" | \ \  / /   //  //  / /\ \\ \  25|")  
-                print(" | _\ \/ /___/ \_//  / /  \/_\ \ []|") 
-                print(" | \__/\____/\___/   \/     \__/ []|")
-                print(" |===_______===_______===_______===|")
-                print(" ||*| _____ |*| _____ |*| _____ |*||")
-                print(" ||*||     ||*||     ||*||     ||*||")
-                print(" ||*|| " + game[0] + "   ||*|| " + game[1] + "   ||*|| " + game[2] + "   ||*||")
-                print(" ||*||_____||*||_____||*||_____||*|| __")
-                print(" ||*|_______|*|_______|*|_______|*||(__)")
-                print(" |===_______===_______===_______===| ||")
-                print(" ||*| _____ |*| _____ |*| _____ |*|| ||")
-                print(" ||*||     ||*||     ||*||     ||*|| ||")
-                print(" ||*|| " + game[0] + "   ||*|| " + game[1] + "   ||*|| " + game[2] + "   ||*|| ||")
-                print(" ||*||_____||*||_____||*||_____||*|| ||")
-                print(" ||*|_______|*|_______|*|_______|*||_//")
-                print(" |===_______===_______===_______===|_/")
-                print(" ||*|  ___  |*|   |   |*| _____ |*||")
-                print(" ||*| |_  | |*|  / \  |*||     ||*||")
-                print(" ||*|  / /  |*| /_ _\ |*||*BAR*||*||")             
-                print(" ||*| /_/   |*|   O   |*||_____||*||")       
-                print(" ||*|_______|*|_______|*|_______|*||")
-                print(" |lc=___________________________===|")
-                print(" |  /___________________________\  |")
-                print(" |   |                         |   |")
-                print("_|    \_______________________/    |_")
-                print("(_____________________________________)")
-                # print("{ [[" + game[0] + " ]] [[" + game[1] + " ]] [[" + game[2] + " ]] } / ")
-            else:
-                print("You don't have enough coins")
-                coinsInserted  = False
+print("\n|===_________===_______===_________===")
+print("|*| Welcome to Python Slot Machine |*|")
+print("|===_________===_______===_________===\n")
 
+playingGame = False
+while playingGame is False:
+        print("Pres [[1]] to Play [[0]] to Exit or [[2]] to see the Rules\n")
+        option = int(input("Option: "))
+        if option == 2:
+            print("Rules\n")
+            playingGame = False
+        elif option == 1:
+            playingGame = True
+            coinsInserted  = False
+            while coinsInserted  is False:
+                print("\nYou have " + str(coins) + " coins to play")
+                print("\nHow many coins do you want to bet on the line?")
+                bet = int(input("BET: "))
+                if bet <= 0:
+                    print("\nInvalid value, try again")
+                    coinsInserted  = False
+                elif bet <= coins:
+                    coinsInserted  = True
+                    coins = coins - bet
+                    game = [reel1[rd.randint(0,20)],reel2[rd.randint(0,23)],reel3[rd.randint(0,22)]]
+                    print("\n              .-------.")
+                    print("              |       |")
+                    print("  ____________|_______|____________")
+                    print(" |  __    __    ___  _____   __    |")  
+                    print(" | / _\  / /   /___\/__   \ / _\   |") 
+                    print(" | \ \  / /   //  //  / /\ \\ \  25|")  
+                    print(" | _\ \/ /___/ \_//  / /  \/_\ \ []|") 
+                    print(" | \__/\____/\___/   \/     \__/ []|")
+                    print(" |===_______===_______===_______===| __")
+                    print(" ||*|_______|*|_______|*|_______|*||(__)")
+                    print(" |===_______===_______===_______===| ||")
+                    print(" ||*| _____ |*| _____ |*| _____ |*|| ||")
+                    print(" ||*||     ||*||     ||*||     ||*|| ||")
+                    print(" ||*|| " + game[0] + "   ||*|| " + game[1] + "   ||*|| " + game[2] + "   ||*|| ||")
+                    print(" ||*||_____||*||_____||*||_____||*|| ||")
+                    print(" ||*|_______|*|_______|*|_______|*||_//")
+                    print(" |===_______===_______===_______===|_/")      
+                    print(" |lc=___________________________===|")
+                    print(" |  /___________________________\  |")
+                    print(" |   |                         |   |")
+                    print("_|    \_______________________/    |_")
+                    print("(_____________________________________)\n")
+                else:
+                    print("You don't have enough coins")
+                    coinsInserted  = False
+        elif option == 0:
+            print("\n Bye!")
+            break
+        else:
+            print("\nInvalid value, try again")
+            playingGame  = False
 
-playSlotMachine(coins = initialCoins)
