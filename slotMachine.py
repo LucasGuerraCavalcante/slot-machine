3ed66294f9412cc37b4ee29495f2cee07e8c1292
LucasGuerraCavalcante <lucasguerratee@gmail.com>
2019-07-30 22:24:33 -0300

    Adding the Emojis and the Machine

--- slotMachine.py
+++ slotMachine.py
@@ -1,19 +1,34 @@
-import random
+# -*- coding: utf-8 -*-
+import os, sys
+import random as rd
 
 initialCoins = 50
 
 def playSlotMachine(coins):
 
-    reel1 = ["7", "BAR", "BAR", "BAR", "WM", "WM", "BELL", "GP", "GP", "GP", "GP", "GP", "GP", "GP", "OR", "OR", "OR",
-            "OR", "OR", "CH", "CH"]
+    reel1 = ["🐍", # Python Snake (1)
+            "🤖", "🤖", "🤖", # Robot (3)
+            "🍉", "🍉", # Watermelon (2)
+            "🔔", # Bell (1)
+            "🍇", "🍇", "🍇", "🍇", "🍇", "🍇", "🍇", # Grape (7)
+            "🍊", "🍊", "🍊","🍊", "🍊", # Orange (5)
+            "🍒", "🍒"] # Cherrie (2)
 
-    reel2 = ["7", "BAR", "BAR", "WM", "WM", "BELL", "BELL", "BELL", "BELL", "BELL", "GP", "GP", "GP", "OR", "OR", "OR",
-            "OR", "OR", "CH", "CH",
-            "CH", "CH", "CH", "CH"]
+    reel2 = ["🐍", # Python Snake (1)
+            "🤖", "🤖", # Robot (2)
+            "🍉", "🍉", # Watermelon (2)
+            "🔔", "🔔", "🔔", "🔔", "🔔", # Bell (5)
+            "🍇", "🍇", "🍇", # Grape (3)
+            "🍊", "🍊", "🍊","🍊", "🍊", # Orange (5)
+            "🍒", "🍒","🍒", "🍒", "🍒", "🍒"] # Cherrie (6)
 
-    reel3 = ["7", "BAR", "WM", "WM", "BELL", "BELL", "BELL", "BELL", "BELL", "BELL", "BELL", "BELL", "GP", "GP", "GP",
-            "OR", "OR", "OR", "OR",
-            "LM", "LM", "LM", "LM"]
+    reel3 = ["🐍", # Python Snake (1)
+            "🤖", # Robot (1)
+            "🍉", "🍉", # Watermelon (2)
+            "🔔", "🔔", "🔔", "🔔", "🔔", "🔔", "🔔", "🔔", # Bell (8)
+            "🍇", "🍇", "🍇", # Grape (3)
+            "🍊", "🍊", "🍊", "🍊", # Orange (4)
+            "👾", "👾", "👾", "👾"] # Evil Alien (4)
 
     game = []
 
@@ -25,13 +40,44 @@ def playSlotMachine(coins):
             print("\nHow many coins do you want to bet on the line?")
             bet = int(input("BET: "))
             if bet <= 0:
-                print("Invalid value, try again")
+                print("\nInvalid value, try again")
                 coinsInserted  = False
             elif bet <= coins:
                 coinsInserted  = True
                 coins = coins - bet
-                game = [reel1[random.randint(0,20)],reel2[random.randint(0,23)],reel3[random.randint(0,22)]]
-                return print(game), print(coins)
+                game = [reel1[rd.randint(0,20)],reel2[rd.randint(0,23)],reel3[rd.randint(0,22)]]
+                print("              .-------.")
+                print("              |Jackpot|")
+                print("  ____________|_______|____________")
+                print(" |  __    __    ___  _____   __    |")  
+                print(" | / _\  / /   /___\/__   \ / _\   |") 
+                print(" | \ \  / /   //  //  / /\ \\ \  25|")  
+                print(" | _\ \/ /___/ \_//  / /  \/_\ \ []|") 
+                print(" | \__/\____/\___/   \/     \__/ []|")
+                print(" |===_______===_______===_______===|")
+                print(" ||*|\_     |*| _____ |*|\_     |*||")
+                print(" ||*|| \ _  |*||     ||*|| \ _  |*||")
+                print(" ||*| \_(_) |*||*BAR*||*| \_(_) |*||")
+                print(" ||*| (_)   |*||_____||*| (_)   |*|| __")
+                print(" ||*|_______|*|_______|*|_______|*||(__)")
+                print(" |===_______===_______===_______===| ||")
+                print(" ||*| _____ |*| _____ |*| _____ |*|| ||")
+                print(" ||*||     ||*||     ||*||     ||*|| ||")
+                print(" ||*|| " + game[0] + "   ||*|| " + game[1] + "   ||*|| " + game[2] + "   ||*|| ||")
+                print(" ||*||_____||*||_____||*||_____||*|| ||")
+                print(" ||*|_______|*|_______|*|_______|*||_//")
+                print(" |===_______===_______===_______===|_/")
+                print(" ||*|  ___  |*|   |   |*| _____ |*||")
+                print(" ||*| |_  | |*|  / \  |*||     ||*||")
+                print(" ||*|  / /  |*| /_ _\ |*||*BAR*||*||")             
+                print(" ||*| /_/   |*|   O   |*||_____||*||")       
+                print(" ||*|_______|*|_______|*|_______|*||")
+                print(" |lc=___________________________===|")
+                print(" |  /___________________________\  |")
+                print(" |   |                         |   |")
+                print("_|    \_______________________/    |_")
+                print("(_____________________________________)")
+                # print("{ [[" + game[0] + " ]] [[" + game[1] + " ]] [[" + game[2] + " ]] } / ")
             else:
                 print("You don't have enough coins")
                 coinsInserted  = False
