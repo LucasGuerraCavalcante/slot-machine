cde10797b70bb85a220b2f20980bc365bd8b8796
Lucas Guerra <lucasguerratee@gmail.com>
2019-09-23 12:38:17 -0300

    now its finaly playable :)

--- app/play.py
+++ app/play.py
@@ -5,15 +5,23 @@ from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
+class coins():
+    coins = None
+
+data = coins()
+
 @app.route('/')
 def render_static():
 
-    return render_template('index.html')
+    data.coins= 50
+
+    return render_template('index.html', status='Bet some coins', 
+    a=' ', b=' ', c=' ', coins=data.coins)
 
 
 @app.route("/bet", methods=['GET','POST'])
 def bet():
-    
+
     reel1 = [
                 "7", # 7 (1)
                 "A", "A", "A", # A (bar) (3)
@@ -41,62 +49,60 @@ def bet():
                 "♦", "♦", "♦", "♦", # Diamonds (Orange) (4)
                 "☹", "☹", "☹", "☹"] # Sad Face (Lemon) (4)
 
-    coins = 50
-    
     bet = request.form['betInput']
     bet = int(bet)
 
-    coins -= bet
+    if (data.coins and bet) and (data.coins > 0 and bet > 0):
 
-    if (coins and bet) and (coins > 0 and bet > 0):
+        data.coins -= bet
 
         game = [reel1[rd.randint(0,20)],reel2[rd.randint(0,23)],reel3[rd.randint(0,22)]]
         if game == ["7","7","7"]:
             prize = bet*200
             status = "YOU WON " + str(prize) +" coins!!!"
-            coins += prize
+            data.coins += prize
             a = game[0]
             b = game[1]
             c = game[2]
         elif game == ["A", "A", "A"] or game == ["♠", "♠", "♠"] or game == ["♠", "♠", "A"]:
             prize = bet*100
             status = "YOU WON " + str(prize) +" coins!!!"
-            coins += prize
+            data.coins += prize
             a = game[0]
             b = game[1]
             c = game[2]
         elif game == ["♣","♣","♣"] or game == ["♣","♣", "A"]:
             prize = bet*18
             status = "YOU WON " + str(prize) +" coins!!!"
-            coins += prize
+            data.coins += prize
             a = game[0]
             b = game[1]
             c = game[2]
         elif game == ["♥", "♥", "♥"] or game == ["♥", "♥", "A"]:
             prize = bet*14
             status = "YOU WON " + str(prize) +" coins!!!"
-            coins += prize
+            data.coins += prize
             a = game[0]
             b = game[1]
             c = game[2]
         elif game == ["♦", "♦", "♦"] or game == ["♦", "♦", "A"]:
             prize = bet*10
             status = "YOU WON " + str(prize) +" coins!!!"
-            coins += prize
+            data.coins += prize
             a = game[0]
             b = game[1]
             c = game[2]
         elif game[0] == "☺" and  game[1] == "☺":
             prize = bet*5
             status = "YOU WON " + str(prize) +" coins!!!"
-            coins += prize
+            data.coins += prize
             a = game[0]
             b = game[1]
             c = game[2]
         elif game[0] == "☺":
             prize = bet*2
             status = "YOU WON " + str(prize) +" coins!!!"
-            coins += prize
+            data.coins += prize
             a = game[0]
             b = game[1]
             c = game[2]
@@ -110,35 +116,22 @@ def bet():
         print(a)
         print(b)
         print(c)
-        print(coins)
+        print(data.coins)
 
-        return render_template('index.html', status=status, a=a, b=b, c=c, coins=coins)
-
-        '''return jsonify({
-            'coins': coins,
-            'a': a,
-            'b': b,
-            'c': c,
-            'status': status,
-        })'''
+        return render_template('index.html', status=status, 
+        a=a, b=b, c=c, coins=data.coins)
 
     else:
-        if (coins < 0):
-            status = "Missing coins"
+        if (data.coins < 0):
+            status = "Out of coins - Restart the server to play again"
         elif (bet < 0):
             status = "Missing a resonable bet value"
         else: 
             status = "Missing data"
 
-        return render_template('index.html', status=status, a='X', b='X', c='X', coins=coins)
+        return render_template('index.html',
+            status=status, a='X', b='X', c='X', coins=data.coins)
 
-        '''return jsonify({
-            'coins': coins,
-            'a': 'X',
-            'b': 'X',
-            'c': 'X',
-            'status': status,
-        })'''
-        
+            
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port='5000', debug=True)
